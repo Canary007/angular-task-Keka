e7579d176b31f8cc7998dd1bd1cb392f7fef5ea3
Canary007 <72484182+Canary007@users.noreply.github.com>
2022-10-26 17:31:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-task-Keka
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mgh794)
---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  public people: any[];
+  public tasks: any[];
+
+  public status = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getTasks();
+    // this.getPeople();
+  }
+
+  public getTasks(): void {
+    let header = new HttpHeaders();
+    header.set('Content-Type', 'application/json');
+    this.http
+      .get('./../assets/task.json', { headers: header })
+      .subscribe((obj: any) => {
+        this.tasks = obj;
+
+        this.http
+          .get('./../assets/status.json', { headers: header })
+          .subscribe((response: any) => {
+            this.status = response;
+          });
+        this.http
+          .get('./../assets/people.json', { headers: header })
+          .subscribe((res: any) => {
+            this.people = res;
+
+            this.tasks.forEach((item) => {
+              const found = this.people.find(
+                (ele) => ele.id === item.assignedTo
+              );
+              item.firstName = found?.firstName;
+              item.lastName = found?.lastName;
+
+              const foundStatus = this.status.find(
+                (ele2) => ele2.id === item.statusId
+              );
+              item.status = foundStatus.name;
+            });
+          });
+      });
+  }
+}
